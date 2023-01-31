9334abf7847c141c1b1a8146895ce7d0f4d86b55
sun0te <jin1123a@gmail.com>
2023-01-31 19:11:18 +0900

    JDBC실습_mysql 연동

---
+++ JDBC/src/JDBCExample1.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCExample1 {
+	public static void main(String args[]) {
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees",
+		"root", "123456");
+			System.out.println("데이터베이스에 접속했습니다.");
+			conn.close();
+		}
+		
+		catch (ClassNotFoundException cnfe) {
+			System.out.println("해당 클래스를 찾을 수 없습니다." + cnfe.getMessage());
+		}
+		
+		catch (SQLException se) {
+			System.out.println(se.getMessage());
+		}
+		
+	}
+}
---
+++ JDBC/src/SelectExam.java
@@ -0,0 +1,56 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SelectExam {
+
+	public static void main(String[] args) {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/tabledb?serverTimezone=UTC";
+		String id = "root";
+		String pw = "123456";
+		Connection con = null;
+		Statement stmt  = null;
+		ResultSet rs = null;
+		
+		String sql = "Select * from usertbl";
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, id, pw);
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			System.out.println("userID\t name\t brithYear\t addr\t mobile1\t mobile2\t height\t mDate");
+			System.out.println("-----------------------------------------------------------------------------------------------------");
+		
+			while(rs.next()) {
+				System.out.print(rs.getString("userID") + "\t ");
+				System.out.print(rs.getString("name") + "\t ");
+				System.out.print(rs.getInt("birthYear") + "\t\t ");
+				System.out.print(rs.getString("addr") + "\t ");
+				System.out.print(rs.getString("mobile1") + "\t\t ");
+				System.out.print(rs.getString("mobile2") + "\t ");
+				System.out.print(rs.getInt("height") + "\t ");
+				System.out.println(rs.getString("mDate") + "\t");
+			}
+		}
+		catch (Exception e) {
+			System.out.println("데이터베이스 연결 실패 !");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(con != null) con.close();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		
+	}
+
+}
